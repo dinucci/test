b3a3abc9bf798993371f74cc172e33a002e87c0c
Ale DN <dinucci@protonmail.ch>
2020-04-24 17:33:48 +0200

    Add files via upload
    Requirements:
    - Ubuntu 18.04
    - Internet connection
    
    What it does:
    - Adds VirtualBox repos to /etc/apt/sources.list for Ubuntu 18.04 LTS
    - Updates & Upgrade OS
    - Verify Virtualization is enabled in the CPU.
         - Intel = VMX * It is a known issue some older Intel CPUs wich supports VMX have VirtualBox compatibility.
         - AMD = SVM
    - Install VirtualBox
    - Install Kubectl
    - Show Kubectl Version After Install
    - Install Minikube
    - Starts Minikube with minikube start --driver=virtualbox
    
    If driver fails you can try with the Docker version

---
+++ minikube-vbox-ubuntu1804-setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -euo pipefail
+
+## Add VirtualBox repos
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian bionic contrib" | sudo tee -a /etc/apt/sources.list
+
+## Update OS
+
+sudo apt update && sudo apt upgrade -y
+
+## Verify VMX/SVM
+grep -E --color 'vmx|svm' /proc/cpuinfo
+
+## Install VirtualBox
+sudo apt install virtualbox-6.1 -y
+
+
+## Install Kubectl
+## https://kubernetes.io/docs/tasks/tools/install-kubectl/#install-kubectl-on-linux
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+
+chmod +x ./kubectl
+
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+kubectl version --client
+
+##  Install Minikube
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+  && chmod +x minikube
+
+sudo mkdir -p /usr/local/bin/
+sudo install minikube /usr/local/bin/
+
+minikube start --driver=virtualbox
+
