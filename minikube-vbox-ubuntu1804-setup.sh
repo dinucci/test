c26120a2357eae065aa9b19f6f2adfe450ff5f48
Ale DN <dinucci@protonmail.ch>
2020-04-24 13:21:28 -0300

    Update minikube-vbox-ubuntu1804-setup.sh
    Removed VirtualBox installed, moved to a separate script.

--- minikube-vbox-ubuntu1804-setup.sh
+++ minikube-vbox-ubuntu1804-setup.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-## Add VirtualBox repos
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian bionic contrib" | sudo tee -a /etc/apt/sources.list
-
-## Update OS
-
-sudo apt update && sudo apt upgrade -y
-
-## Verify VMX/SVM
-grep -E --color 'vmx|svm' /proc/cpuinfo
-sleep 5
-
-## Install VirtualBox
-sudo apt update
-sleep 1
-sudo apt install virtualbox-6.1 -y
-sleep 5
-
 ## Install Kubectl
 ## https://kubernetes.io/docs/tasks/tools/install-kubectl/#install-kubectl-on-linux
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
