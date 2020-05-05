f8720ec3648ac1a97c56e79f3fc477679ad24e18
Ale DN <dinucci@protonmail.ch>
2020-05-05 09:18:57 -0300

    Update base-docker-setup-ubuntu2004.sh

--- base-docker-setup-ubuntu2004.sh
+++ base-docker-setup-ubuntu2004.sh
@@ -4,7 +4,7 @@ echo "Ubuntu 20.04 LTS Update & Upgrade"
 sudo apt update && sudo apt upgrade -y
 
 echo "Install Base Tools"
-sudo apt install -y git curl make htop docker.io docker-compose docker docker-engine
+sudo apt install -y git curl htop docker.io docker-compose docker 
 
 # echo "usermod"
 sudo usermod -aG docker $USER
