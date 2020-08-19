0d14ba60d770f10ad332d4464bdac68e12d122bc
Ale DN <dinucci@protonmail.ch>
2020-08-18 21:39:51 -0300

    Create manjaro001-ssh.sh
    Manjaro post install

---
+++ manjaro001-ssh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo systemctl status sshd.service
+sudo systemctl enable sshd.service
+sudo systemctl start sshd.service
+sudo systemctl status sshd.service
+
+
+sudo ufw status
+sudo ufw enable
+sudo ufw allow 22
+sudo ufw status
+
